f1d8a24266644b18c856b8305a6733977067ca8d
rakib2067 <rakib7@hotmail.co.uk>
2021-09-25 18:59:17 +0100

    filtered list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,16 +11,20 @@ export default function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpensesFilter current={selected} currentSelected={currentSelected} />
-      {props.expenses.map((item) => {
-        return (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        );
-      })}
+      {props.expenses
+        .filter((item) => {
+          return item.date.getFullYear() == selected;
+        })
+        .map((item) => {
+          return (
+            <ExpenseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          );
+        })}
     </Card>
   );
 }
