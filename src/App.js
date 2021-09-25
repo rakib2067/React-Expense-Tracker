1190f440e391ab4ceb131dfc2e94df81fccb65a9
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-09-25 17:45:22 +0100

    lists and keys

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import ExpenseForm from "./components/NewExpense/ExpenseForm";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_DATA = [
+  {
+    id: Math.random().toString(),
+    title: "Car Insurance",
+    amount: 300,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: Math.random().toString(),
+    title: "Health Insurance",
+    amount: 250,
+    date: new Date(2011, 3, 15),
+  },
+  {
+    id: Math.random().toString(),
+    title: "Dental Insurance",
+    amount: 3000,
+    date: new Date(2019, 5, 9),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: Math.random().toString(),
-      title: "Car Insurance",
-      amount: 300,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: Math.random().toString(),
-      title: "Health Insurance",
-      amount: 250,
-      date: new Date(2011, 3, 15),
-    },
-    {
-      id: Math.random().toString(),
-      title: "Dental Insurance",
-      amount: 3000,
-      date: new Date(2019, 5, 9),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
   const addExpense = (expense) => {
-    console.log("This is APP JS");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,18 +3,13 @@ import Card from "../UI/Card";
 import Date from "./Date";
 import "./ExpenseItem.css";
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  const handleClick = () => {
-    setTitle("This  is title number: " + Math.random());
-  };
   return (
     <Card className="expense-item">
       <Date date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 }
